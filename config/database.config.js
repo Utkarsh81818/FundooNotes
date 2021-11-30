70be0f8241afddcfcc4eba81f3f9c6e06553ae1a
unknown <utkarshkr945@gmail.com>
2021-12-01 00:17:11 +0530

    Added: dotEnv in Fundoo Notes

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-    url: 'mongodb://localhost:27017/fundoo-notes'
+const mongoose = require('mongoose');
+require('dotenv').config();
+class DBconnection {
+    connection = () => {
+        const url = process.env.URL;
+        mongoose.connect(url, {
+        }).then(() => {
+            console.log("Successfully connected to the database");
+        }).catch(err => {
+            console.log('Could not connect to the database. Exiting now...', err);
+            process.exit();
+        });
+    }
 }
+module.exports = new DBconnection();
--- server.js
+++ server.js
@@ -1,32 +1,29 @@
 const express = require('express');
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
+require('dotenv').config();
 
 // create express app
 const app = express();
 
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
 app.use(express.json())
 
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+
 // Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-
-}).then(() => {
-    console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
-
+dbConfig.connection();
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
+    res.json({ "message": "Welcome to fundooNotes application. Take notes quickly. Organize and keep track of all your notes." });
 });
-
 // Require Notes routes
 require('./app/routes/note.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log("Server is listening");
 });
+module.exports = app
